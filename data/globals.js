37a2cc10dbe3953f6098c4bbbd5b0f31bccfa45d
Sebastin M. Perez <perezsebastianm@gmail.com>
2015-05-05 12:08:11 -0300

    Add globals file

---
+++ data/globals.js
@@ -0,0 +1,14 @@
+module.exports = {
+    'host': '',
+    'urls': {
+        'desktop':{
+            'home':'http://www.mercadolibre.com.ar'
+        },
+        'mobile': {
+
+        },
+    },
+    'mock':{
+        'image': require('path').resolve(__dirname + '/../assets/images/item-de-test.jpg')
+    }
+};
